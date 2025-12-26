0460f397f4e0aaab0c5ad2f73f928cb587350c1c
BigFudge420 <87513995+BigFudge420@users.noreply.github.com>
2025-12-27 03:40:13 +0530

    feat : add controller logic

--- src/proxyController.ts
+++ src/proxyController.ts
@@ -1,8 +1,18 @@
 import { Request, Response, NextFunction } from "express"
+import { tryConsume } from "./tokenLogic"
+import handleUpstream from "./handleUpstream"
+import { enqueue } from "./enqueueLogic"
+import processQueue from "./processQueue"
 
-const proxyController = (req : Request, res : Response, next : NextFunction) => {
+const proxyController = (req : Request, res : Response) => {
+    if (tryConsume()) {
+        handleUpstream(req, res)
+    }
+    else {
+        enqueue(req, res)
+    }
 
-
+    processQueue()
 }
 
 export default proxyController
